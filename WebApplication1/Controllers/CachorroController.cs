29207b35b288b3c7da77421d7415c34eb6b76d14
paolacostxx <paolacosta599@gmail.com>
2023-10-03 12:44:13 -0300

    Primeiro commit

---
+++ WebApplication1/Controllers/CachorroController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CachorroController : Controller
+    {
+        //GET: Cachorro
+        public ActionResult Index()
+        {
+            return View(CachorroModel.CriarLista());
+        }
+
+        // GET: Cachorro/Details/5
+        public ActionResult Details(int id)
+        {
+            var dog = CachorroModel.CriarLista()[id];
+            return View(dog);
+        }
+
+        // GET: Cachorro/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cachorro/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cachorro/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Cachorro/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cachorro/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cachorro/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/CachorroModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CachorroModel
+    {
+        public string Raca { get; set; }
+        public string Porte { get; set; }
+        public string Nacionalidade { get; set; }
+        public string Ano { get; set; }
+
+        public static List<CachorroModel> CriarLista()
+        {
+            var lista = new List<CachorroModel>();
+
+            lista.Add(new CachorroModel() { Raca = "Border Collie", Porte = "Médio", Nacionalidade = "Escócia", Ano = "10 a 17 anos" });
+            lista.Add(new CachorroModel() { Raca = "Pastor Alemão", Porte = "Grande", Nacionalidade = "Alemanha", Ano = "9 a 13 anos" });
+            lista.Add(new CachorroModel() { Raca = "Pinsher", Porte = "Pequeno", Nacionalidade = "Alemanha", Ano = "12 a 16 anos" });
+            lista.Add(new CachorroModel() { Raca = "Vira lata caramelo", Porte = "Pequeno", Nacionalidade = "Brasil", Ano = "15 anos" });
+            lista.Add(new CachorroModel() { Raca = "Pug", Porte = "Pequeno", Nacionalidade = "China", Ano = "12 a 15 anos" });
+
+            return lista;
+        }
+    }
+}
